bd28fe82aec344a4bdd9701e195d39fc7455774b
Joney Xiao <joney_xiao@epam.com>
2025-06-08 21:17:07 +0800

    fix: next.js config for vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
 import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
-  output: "standalone",
-  distDir: process.env.NODE_ENV === "production" ? ".next-prod" : ".next",
   typescript: {
     ignoreBuildErrors: true
   }
